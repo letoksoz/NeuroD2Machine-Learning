344ec198fb8e7e8e63bb187d1dc95be572b612ce
Levent Toksoz <letoksoz@umich.edu>
2018-05-07 12:54:27 +0300

    added bn
    added bn

--- CNN.jl
+++ CNN.jl
@@ -35,14 +35,13 @@ function generateWindowedData(x,y,indexList,window_size)
 
     for n = -window_shift:window_shift
         m = x[:,(window_shift+1+n):(sample_size-window_shift+n)]
-        #push!(windows[n+window_shift+1,:,:],  m[:,indexList_x])
         windows[n+window_shift+1,1,:,:] = m[:,indexList_x]
     end
     y = y[:,indexList]
     return windows, y
 end
 
-function prepareData(fileprefix,window_size)
+function prepareData(fileprefix,window_size,batchsize)
     ##Class 1---Without NeuroD2, Class 2---With NeuroD2
     data = load("levent.jld2","data")
 
@@ -58,7 +57,6 @@ function prepareData(fileprefix,window_size)
 
     y_data = Array{Int64}(y_data)
     y_data = 1 + vec(y_data)
-
 
     #Creating Shuffled IndexList
     num_samples = length(balanced_index_list)
@@ -77,15 +75,8 @@ function prepareData(fileprefix,window_size)
     windowed_chr_test = windowed_chr_data[:,:,:,test_index_list]
     y_test = y_data[test_index_list]
 
-    global dtrn = Any[]
-    for i = 1:100:(length(y_trn)-100)
-        push!(dtrn, (windowed_chr_trn[:,:,:,i:i+99],y_trn[i:i+99]))
-    end
-
-    global dtst = Any[]
-    for i = 1:100:(length(y_test)-100)
-        push!(dtst, (windowed_chr_test[:,:,:,i:i+99],y_test[i:i+99]))
-    end
+    global dtrn = minibatch(windowed_chr_trn, y_trn, batchsize;shuffle=true,xtype=KnetArray{Float32})
+    global dtst = minibatch(windowed_chr_test, y_test, batchsize;shuffle=true,xtype=KnetArray{Float32})
 end
 ####################################################################
 
@@ -94,35 +85,47 @@ function sl()
     println("******************************************************************")
 end
 
+function conv_layer(w, m, x)
+    cl = conv4(w[1], x; padding=(2,1), stride=(2,1))
+    cl = batchnorm(cl, m, w[2])
+    cl = relu.(cl)
+    return cl
+end
+
+function fc_layer(w, m, x)
+    fc = w[1] * mat(x)
+    fc = batchnorm(fc, m, w[2])
+    return relu.(fc)
+end
+
 #=
 The trained architecture has the form of:
 conv -> conv -> conv ->  Avg.pool -> fc -> output
 =#
 
-function predict_CNN(w,x)
+function predict(w, m, x)
     # conv. layer
-    x = relu.(conv4(w[1], x; padding=(2,1), stride=(2,1)) .+ w[2])
+    x = conv_layer(w[1:2], m[1], x)
     # conv. layer
-    x = relu.(conv4(w[3], x; padding=(2,1), stride=(2,1)) .+ w[4])
+    x = conv_layer(w[3:4], m[2], x)
     # conv. layer
-    x = relu.(conv4(w[5], x; padding=(2,1), stride=(2,1)) .+ w[6])
+    x = conv_layer(w[5:6], m[3], x)
     #global avg.pool layer
     x = pool(x,window=7,mode=1)
     #fc layer
-    x = relu.(w[7] * mat(x) .+ w[8])
+    x = fc_layer(w[7:8], m[4], x)
     return w[9] * mat(x) .+ w[10]
 end
 
-loss_CNN(w,x,y_actual) = nll(predict_CNN(w,x),y_actual)
-lossgradient_CNN = grad(loss_CNN)
+loss(w, m, x, y_actual) = nll(predict(w, m, x), y_actual)
+lossgradient = grad(loss)
 
-function train(w, dtrn; lr=.01, epochs=10)
+function train(w, m, dtrn; lr=.01, epochs=10)
     optim = optimizers(w, Adam; lr=lr)
     println("Starting the epoch 1:")
     for epoch=1:epochs
         for (x,y) in dtrn
-            x = KnetArray{Float32}(x)
-            g = lossgradient_CNN(w, x, y)
+            g = lossgradient(w, m, x, y)
             update!(w,g,optim)
         end
 
@@ -134,13 +137,27 @@ function train(w, dtrn; lr=.01, epochs=10)
     return w
 end
 
-function initialize_weight_CNN()
-    weight = [ xavier(Float32,5,1,15,64),  zeros(Float32,1,1,64,1),
-               xavier(Float32,5,1,64,64), zeros(Float32,1,1,64,1),
-               xavier(Float32,5,1,64,64),  zeros(Float32,1,1,64,1),
-               xavier(Float32,512,64),  zeros(Float32,512,1),
+function initialize_weight()
+    weight = [ xavier(Float32,5,1,15,64),  bnparams(Float32, 64),
+               xavier(Float32,5,1,64,64), bnparams(Float32, 64),
+               xavier(Float32,5,1,64,64),  bnparams(Float32, 64),
+               xavier(Float32,512,64),  bnparams(Float32, 512),
                xavier(Float32,2,512),  zeros(Float32,2,1)]
     return map(KnetArray{Float32}, weight)
+end
+
+function initialize_model()
+    # Initializing weights
+    w = initialize_weight()
+    # Initializing a moments object/batchnorm
+    m = Any[bnmoments() for i = 1:4]
+    return w,m
+end
+
+# Accuracy computation
+function compute_accuracy(w, m, data=dtst)
+    model = (w, m)
+    return accuracy(model, data, (model, x)->predict(model[1], model[2], x); average=true)
 end
 
 function print_weight(message, weight)
@@ -160,22 +177,11 @@ end
 #############################Main################################
 fileprefix = "chr1"
 batchsize = 100
-
-#window_size_list = [1,3,5,7,9,11,13,15,17,19,25,31,39,45,51,55,61,67]
-#trn_accuracy_values = Any[]
-#tst_accuracy_values = Any[]
 window_size = 11
-prepareData(fileprefix,window_size)
-w = initialize_weight_CNN()
-w = train(w, dtrn; lr=.01, epochs=10)
-accuracy(w,dtrn,predict_CNN)
 
-push!(trn_accuracy_values, accuracy(w,dtrn,predict))
-push!(tst_accuracy_values, accuracy(w,dtst,predict))
-println("Training and Test Accuracy Values Are Calculated for the Window Size:",1)
+prepareData(fileprefix,window_size,batchsize)
+w, m = initialize_model()
+w = train(w, m, dtrn; lr=.01, epochs=10)
 
-
-trn_accuracy_values
-tst_accuracy_values
+compute_accuracy(w, m)
 #################################################################
-#trn_loss
